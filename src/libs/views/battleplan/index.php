94c80e55f6a1f6c47a92b532b0c22b565bfeb1a2
Shinya <slughedgehog@gmail.com>
2026-07-22 16:22:39 +0900

    バトルプラン変更

--- src/libs/views/battleplan/index.php
+++ src/libs/views/battleplan/index.php
@@ -1,20 +1,31 @@
+<?php
+$groups = array_chunk($battleplans ?? [], 6);
+?>
 <div class="battleplan index">
 	<h1 class="battleplan-page-title">バトルプラン</h1>
 
 	<?php if (empty($battleplans)): ?>
 		<p class="battleplan-empty">バトルプランが登録されていません。</p>
 	<?php else: ?>
-		<ul class="battleplan-list">
-			<?php foreach ($battleplans as $bp): ?>
-				<li>
-					<a href="<?= URL; ?>battleplan/detail/<?= (int)$bp['id']; ?>" class="battleplan-list-item">
-						<span class="battleplan-list-name"><?= $this->h($bp['name']); ?></span>
-						<span class="battleplan-list-meta">
-							<?= (int)($bp['rounds'] ?? 5); ?> ラウンド
-						</span>
-					</a>
-				</li>
+		<div class="battleplan-list-container">
+			<?php foreach ($groups as $groupIndex => $group): ?>
+				<section class="battleplan-group">
+					<h2 class="battleplan-group-title">バトルプラン <?= (int)$groupIndex + 1; ?></h2>
+					<ul class="battleplan-list">
+						<?php foreach ($group as $i => $bp): ?>
+							<li>
+								<a href="<?= URL; ?>battleplan/detail/<?= (int)$bp['id']; ?>" class="battleplan-list-item">
+									<span class="battleplan-list-num"><?= (int)$i + 1; ?></span>
+									<span class="battleplan-list-name"><?= $this->h($bp['name']); ?></span>
+									<span class="battleplan-list-meta">
+										<?= (int)($bp['rounds'] ?? 5); ?> ラウンド
+									</span>
+								</a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</section>
 			<?php endforeach; ?>
-		</ul>
+		</div>
 	<?php endif; ?>
 </div>
